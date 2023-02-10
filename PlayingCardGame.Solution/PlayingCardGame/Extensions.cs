4ea462b41dae1dbc9000b369bf631ebbca1f0c02
Richard Hsu <45455105+bung8833@users.noreply.github.com>
2023-02-10 20:38:13 +0800

    新增擴充方法 Are5ContinuousCards()

--- PlayingCardGame.Solution/PlayingCardGame/Extensions.cs
+++ PlayingCardGame.Solution/PlayingCardGame/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,14 +20,36 @@ namespace PlayingCardGame
             List<int> distincts = source.Distinct().ToList();
 
             // 計算source中 每個數字各出現幾次
-            List<int> appearances = new List<int>();
+            List<int> vAppearances = new List<int>();
             for (int idx = 0; idx < distincts.Count; idx++)
             {
-                int count = source.Count(v => v == distincts[idx]);
-                appearances.Add(count);
+                int vAppears = source.Count(v => v == distincts[idx]);
+                vAppearances.Add(vAppears);
             }
 
-            return appearances.OrderBy(v => v).ToList();
+            return vAppearances.OrderBy(v => v).ToList();
+        }
+
+        /// <summary>
+        /// 判斷傳入的牌是否恰好5張 且數字連續(包含10JQKA)
+        /// </summary>
+        /// <param name="hand"></param>
+        /// <returns></returns>
+        public static bool Are5ContinuousCards(this List<Card> hand)
+        {
+            if (hand.Count != 5) return false;
+
+            // 數字必須皆不相同
+            if (hand.Select(c => c.Value).Distinct().Count() != 5) return false;
+
+            // 5張不同的牌 其數字連續
+            bool continuous = hand.Select(c => c.Value).Max()
+                            - hand.Select(c => c.Value).Min() == 4;
+
+            // 10 J Q K A
+            bool royal = hand.Select(h => h.Value).All(new int[] { 10, 11, 12, 13, 1 }.Contains);
+
+            return continuous || royal;
         }
     }
 }
